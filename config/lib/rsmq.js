d090837cd09b803bbe2d0740652fbf6c7df1fde2
bsimpson53 <ben@hy-street.net>
2017-02-18 10:35:59 +0000

    allow 10min to process a spreadsheet

--- config/lib/rsmq.js
+++ config/lib/rsmq.js
@@ -12,7 +12,7 @@ module.exports.init = function () {
     var worker = new RSMQWorker( 'rsmq_catalogue_'  + process.env.NODE_ENV, {
         host: config.redis.host,
         port: 6379,
-        timeout: 60000
+        timeout: 60000 * 10 // 10 min
     });
     
     module.exports.queue = worker;
